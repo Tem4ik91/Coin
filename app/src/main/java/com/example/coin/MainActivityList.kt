15e3b6a524981ec592a2350ceba5e9edf4785fb6
Артём <60760522+Tem4ik91@users.noreply.github.com>
2020-05-23 15:11:36 +0300

    dinList

--- app/src/main/java/com/example/coin/MainActivityList.kt
+++ app/src/main/java/com/example/coin/MainActivityList.kt
@@ -2,6 +2,9 @@ package com.example.coin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main_list.*
 
 class MainActivityList : AppCompatActivity() {
@@ -10,7 +13,20 @@ class MainActivityList : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_list)
 
+        var arrayList = arrayListOf<String>()
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList)
+        ListView.adapter = adapter
+
+        editTextList2.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                //Perform Code
+                return@OnKeyListener true
+            }
+            false
+        })
         buttonList.setOnClickListener {
+
+
 
 
 
